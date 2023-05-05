52fecfbe5813ded237c5aabf8cc43862cb2bdadd
maliByatzes <malib2027@gmail.com>
2023-05-05 16:21:09 +0200

    save

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,23 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>Hello</h1>
+      <div className='main__div'>
+        <div className='main__div-input'>
+          <div className='main__div-input-day'>
+            <label htmlFor="day__input">day</label>
+            <input type="text" name="day" id="day__input" />
+          </div>
+          <div className='main__div-input-month'>
+            <label htmlFor="month__input">month</label>
+            <input type="text" name="month" id="month__input" />
+          </div>
+          <div className='main__div-input-year'>
+            <label htmlFor="year__input">year</label>
+            <input type="text" name="year" id="year__input" />
+          </div>
+        </div>
       </div>
+
     </>
   )
 }
